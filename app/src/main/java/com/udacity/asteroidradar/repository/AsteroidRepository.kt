905527836cb94737a0d835a491fa6647fb6609a6
ksdc-anandhid <Anandhi.Dharmarajan@ksdc.co.za>
2022-09-08 21:24:04 +0200

    Added Picture of the Day

--- app/src/main/java/com/udacity/asteroidradar/repository/AsteroidRepository.kt
+++ app/src/main/java/com/udacity/asteroidradar/repository/AsteroidRepository.kt
@@ -1,9 +1,7 @@
 package com.udacity.asteroidradar.repository
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
-import androidx.lifecycle.viewModelScope
 import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
 import com.udacity.asteroidradar.database.AsteroidsDatabase
 import com.udacity.asteroidradar.database.asDomainModel
@@ -13,7 +11,6 @@ import com.udacity.asteroidradar.network.AsteroidApi
 import com.udacity.asteroidradar.network.PictureOfDay
 import com.udacity.asteroidradar.util.Util
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
 
@@ -28,8 +25,8 @@ class AsteroidRepository(private val database: AsteroidsDatabase) {
     suspend fun insertAsteroids() {
         withContext(Dispatchers.IO) {
             val response = AsteroidApi.retrofitService.getAsteroidList(
-                Util.Companion.getTodaysDate(),
-                Util.Companion.getTodaysDate(), "ZkPL6anWyY2iJFvTxGJC3XmAKAQU3eegGgohaDFm"
+                Util.Companion.getStartDate(),
+                Util.Companion.getEndDate(), "ZkPL6anWyY2iJFvTxGJC3XmAKAQU3eegGgohaDFm"
             )
             val asteroidList: List<Asteroid> = parseAsteroidsJsonResult(JSONObject(response))
             database.asteroidDao.insertAll(asteroidList.asDatabaseModel())
--- app/src/main/java/com/udacity/asteroidradar/util/Util.kt
+++ app/src/main/java/com/udacity/asteroidradar/util/Util.kt
@@ -1,13 +1,20 @@
 package com.udacity.asteroidradar.util
 
-import java.util.Date
 import java.text.SimpleDateFormat
+import java.util.*
 
 class Util {
     companion object {
-        fun getTodaysDate(): String {
+        fun getStartDate(): String {
             val sdf = SimpleDateFormat("yyyy-MM-dd")
             return sdf.format(Date())
+        }
+
+        fun getEndDate(): String {
+            val cal = Calendar.getInstance()
+            val s = SimpleDateFormat("yyyy-MM-dd")
+            cal.add(Calendar.DAY_OF_YEAR, 7)
+            return s.format(Date(cal.timeInMillis))
         }
     }
 }
