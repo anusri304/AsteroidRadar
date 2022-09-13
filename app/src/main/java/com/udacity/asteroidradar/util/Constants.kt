45556a4bdaa9a39cdd8339a9027e8b2803ebe538
ksdc-anandhid <Anandhi.Dharmarajan@ksdc.co.za>
2022-09-13 08:18:11 +0200

    Removed APIKey constants

--- app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
@@ -4,7 +4,8 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY= "ZkPL6anWyY2iJFvTxGJC3XmAKAQU3eegGgohaDFm"
+    //TODO:Replace API Key
+    const val API_KEY= ""
     const val ASTEROIDS_URL = "neo/rest/v1/feed"
     const val IMAGE_OF_DAY_URL = "planetary/apod"
 }
